65373ae6429c4e122872c73ee6f12cf52754d20a
Ilya Kuprin <lucifer39@bk.ru>
2025-01-05 18:10:50 +0200

    меняю расположение manage.py

--- settings.py
+++ settings.py
@@ -20,7 +20,7 @@ env.read_env()
 TOKEN_BOT = env.str('TOKEN_BOT')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
